212a396bc85742fd4bae57dbc9c3a3ccc63b23e9
austingarci <39674514+austingarci@users.noreply.github.com>
2018-06-26 12:52:29 -0400

    Add files via upload

---
+++ Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Created by Austin Garcia on 6/20/18.
+//  Copyright © 2018 Austin Garcia. All rights reserved.
+
+import Foundation
+
+class Question {
+    
+    let questionText : String
+    let answer : Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+   
+    
+    
+}
